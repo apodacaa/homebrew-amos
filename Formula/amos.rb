a7db271fa7788bef661beed1444310b765a22917
Anthony Apodaca <me@anthonyapodaca.com>
2025-11-04 07:57:10 +0100

    Update to v1.1.0

--- Formula/amos.rb
+++ Formula/amos.rb
@@ -1,17 +1,17 @@
 class Amos < Formula
   desc "Minimal TUI for journal + todo management with brutalist design"
   homepage "https://github.com/apodacaa/amos"
-  url "https://github.com/apodacaa/amos/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "59521ad2ff1e72e0ff00704a4207797f755ff8f5021affa038da365fe8da4ae5"
+  url "https://github.com/apodacaa/amos/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "6c5f0351349b73de0464ecc0a66653b88f077834a4ca545e82315cfbf073af14"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-X main.Version=1.0.0")
+    system "go", "build", *std_go_args(ldflags: "-X main.Version=1.1.0")
   end
 
   test do
-    assert_match "amos version 1.0.0", shell_output("#{bin}/amos --version")
+    assert_match "amos version 1.1.0", shell_output("#{bin}/amos --version")
   end
 end
